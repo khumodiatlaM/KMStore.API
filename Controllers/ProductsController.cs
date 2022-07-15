8a83dc65648cf7657ece5ba193d1db98f0dd306a
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-07-15 13:50:13 +0200

    Add the Products Controller

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,36 @@
+﻿using KMStore.API.Repository;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductsController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var products = await _productRepository.GetProducts();
+
+            return Ok(products);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _productRepository.GetProduct(id);
+
+            if (product == null) return NotFound("No Product match this Id.");
+
+            return Ok(product);
+        }
+    }
+}
