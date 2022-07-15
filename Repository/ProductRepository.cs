2677a1203d7ffc090063437093218813a696983f
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-07-15 13:32:13 +0200

    Add the ProductRepository that implements the IProductRepository

---
+++ Repository/ProductRepository.cs
@@ -0,0 +1,30 @@
+﻿using KMStore.API.Data;
+using KMStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly KMStoreContext _context;
+
+        public ProductRepository(KMStoreContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Product> GetProduct(int id)
+        {
+            return await _context.Products.FindAsync(id);
+        }
+
+        public async Task<ICollection<Product>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
