19704e4b9930bd75e57611f7d493ea405815dc5f
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-07-15 13:20:19 +0200

    Add the IProductRepository Interface

---
+++ Repository/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using KMStore.API.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Repository
+{
+    public interface IProductRepository
+    {
+        Task<ICollection<Product>> GetProducts();
+
+        Task<Product> GetProduct();
+    }
+}
