f431f839ece40d203d2c2924b8847cd8c600839e
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-07-18 23:17:10 +0200

    Add the HttpPost action for the register method

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+﻿using KMStore.API.Models;
+using KMStore.API.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UsersController(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RegisterUser(User user)
+        {
+            var userWithExistingEmail = await _userRepository.CheckUserWithSameEmail(user.Email);
+
+            if (userWithExistingEmail != null)
+            {
+                return BadRequest("User with the same email already exist");
+            }
+            else
+            {
+                await _userRepository.Register(user);
+                return StatusCode(StatusCodes.Status201Created, "User Successfully Added");
+            }
+        }
+    }
+}
