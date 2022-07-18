0ca5b882cc8060a07464b8747ed93a36f854c708
Khumodiatla Moatlhodi <KhumodiatlaM@tfg.co.za>
2022-07-18 23:16:01 +0200

    Add the IUserRepository interface

---
+++ Repository/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using KMStore.API.Models;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Repository
+{
+    public interface IUserRepository
+    {
+        Task Register(User user);
+
+        Task Login(User user);
+
+        Task<User> CheckUserWithSameEmail(string email);
+    }
+}
---
+++ Repository/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using KMStore.API.Data;
+using KMStore.API.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace KMStore.API.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly IConfiguration _configuration;
+        private readonly KMStoreContext _context;
+        //private readonly AuthService _auth;
+
+        public UserRepository(IConfiguration configuration,KMStoreContext context)
+        {
+            _configuration = configuration;
+            _context = context;
+        }
+
+        public async Task<User> CheckUserWithSameEmail(string email)
+        {
+            return await _context.Users.SingleOrDefaultAsync(u => u.Email == email);
+        }
+
+        public async Task Login(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Register(User user)
+        {
+            var userWithExistingEmail = await _context.Users.SingleOrDefaultAsync(u => u.Email == user.Email);
+
+            if(userWithExistingEmail != null)
+            {
+                var userObj = new User
+                {
+                    Name = user.Name,
+                    Email = user.Email,
+                    // Hash Password at later stage
+                    Password = user.Password,
+                    Role = "Users"
+                };
+
+                await _context.Users.AddAsync(userObj);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
